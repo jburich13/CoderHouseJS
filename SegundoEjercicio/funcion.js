0b097325f3ce360964100b6196af3acb08985bd4
Juan Burich <burichojrs@gmail.com>
2020-11-26 20:19:45 -0300

    Ejercicio 2

---
+++ SegundoEjercicio/funcion.js
@@ -0,0 +1,19 @@
+
+var numero =prompt("Ingrese un numero");
+if(numero>1000){
+    alert("El numero es mayor a 1000")
+} else alert("El numero es menor a 1000")
+
+
+var texto  = prompt("Ingrese un texto: ");
+if(texto=="Hola"){
+    console.log("El texto dice 'Hola'");
+} else alert("El texto no dice 'Hola'");
+
+
+var numero2=prompt("Ingrese un numero");
+if(numero2>10){
+    if(numero2<50) alert("El numero esta entra 10 y 50")
+} 
+
+
