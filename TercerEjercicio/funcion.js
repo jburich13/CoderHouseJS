923ae6bec82d0518099ac4423d2e10ab6e3dae66
Juan Burich <burichojrs@gmail.com>
2020-12-02 15:09:40 -0300

    Tercer ejercicio con modif

--- TercerEjercicio/funcion.js
+++ TercerEjercicio/funcion.js
@@ -1,15 +1,12 @@
-let diasLista = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-
-
-function pares() {
+let pares = () => {
+    const diasLista = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     for (let i = 0; i < diasLista.length; i++) {
         if (i == 6) {
-            alert("Hoy es " + diasLista[i]);
+            alert(`Hoy es ${diasLista[i]}`);
         }
         if (i % 2 == 0) {
-            console.log("El dia " + diasLista[i] + " es par");
+            console.log(`El dia ${diasLista[i]} es par`);
         }
     }
 }
-
 pares();
