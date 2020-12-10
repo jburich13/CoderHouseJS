b2824e0906a8895d0b77ff55b9039624e02a1d49
Juan Burich <burichojrs@gmail.com>
2020-12-10 16:21:29 -0300

    agregando poo

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -1,18 +1,28 @@
 let usuario = Number(prompt("Ingrese la cantidad de cursos que desea: "));
-let textos = ["Fotografia", "Programacion", "Testing", "Data Science", "JUnit", "Python for dummies", "Java avanzado"]
-let agregarCards = (titulos, num) => {
-    for (let i = 0; i < usuario; i++) {
+
+class Curso {
+    constructor(titulo, img, descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+}
+let prog = new Curso("Programación", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
+let testing = new Curso("Testing", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
+let diseñoUX = new Curso("Diseño UX", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
+let misCursos = [prog, testing, diseñoUX];
+let agregarCards = () => {
+    misCursos.forEach(card => {
         document.write(" <div class='card' id='card'>")
-        document.write(" <img src='src\\teacher.jpg' class='card-img-top' alt='...'>")
+        document.write(`<img src='src\\${card.img}' class='card-img-top' alt='...'>`)
         document.write(" <div class='card-body'>")
-        document.write(`<h5 class='card-title'>${textos[i]}</h5>`)
-        document.write("  <p class='card-text' id='text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>")
+        document.write(`<h5 class='card-title'>${card.titulo}</h5>`)
+        document.write(`<p class='card-text' id='text'>${card.descripcion}</p>`)
         document.write("<a href='#' class='btn' id='boton'>Go somewhere</a>")
         document.write("</div>")
         document.write("</div>")
-    }
-
+    });
     alert(`Se agregaron ${usuario} tarjetas a la seccion Mis Cursos`)
 }
 
-agregarCards(textos, usuario);
+agregarCards();
