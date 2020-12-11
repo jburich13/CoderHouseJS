285682663192516c833441ac449b76426a06b82f
Juan Burich <burichojrs@gmail.com>
2020-12-10 21:12:56 -0300

    agregando poo 2

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -1,5 +1,7 @@
 let usuario = Number(prompt("Ingrese la cantidad de cursos que desea: "));
 
+
+
 class Curso {
     constructor(titulo, img, descripcion) {
         this.titulo = titulo;
@@ -7,10 +9,19 @@ class Curso {
         this.img = img;
     }
 }
-let prog = new Curso("Programación", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
-let testing = new Curso("Testing", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
-let diseñoUX = new Curso("Diseño UX", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
-let misCursos = [prog, testing, diseñoUX];
+
+
+
+
+
+let misCursos = [
+    new Curso("Programación", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?"),
+    new Curso("Testing", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?"),
+    new Curso("Diseño UX", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?")
+];
+
+
+
 let agregarCards = () => {
     misCursos.forEach(card => {
         document.write(" <div class='card' id='card'>")
@@ -18,7 +29,10 @@ let agregarCards = () => {
         document.write(" <div class='card-body'>")
         document.write(`<h5 class='card-title'>${card.titulo}</h5>`)
         document.write(`<p class='card-text' id='text'>${card.descripcion}</p>`)
-        document.write("<a href='#' class='btn' id='boton'>Go somewhere</a>")
+        document.write("<div class='row'>")
+        document.write("<a href='#' class='btn col-5' id='boton'>Añadir al carrito</a>")
+        document.write("<a href='#' class='btn col-5' id='boton'>Eliminar del carrito</a>")
+        document.write("</div>")
         document.write("</div>")
         document.write("</div>")
     });
