57b0410399ae68814c9ee61471665cfc78a4d4b8
Juan Burich <burichojrs@gmail.com>
2021-01-03 19:42:38 -0300

    agregado que no se muestren numeros negativos

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -1,9 +1,10 @@
 class Curso {
-    constructor(titulo, img, descripcion, precio) {
+    constructor(titulo, img, descripcion, precio, id) {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.img = img;
         this.precio = precio;
+        this.id = id;
 
     }
 }
@@ -13,9 +14,9 @@ class Curso {
 
 
 let misCursos = [
-    new Curso("Programación", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 2000),
-    new Curso("Testing", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 4000),
-    new Curso("Diseño UX", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 5000)
+    new Curso("Programación", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 2000, 1),
+    new Curso("Testing", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 4000, 2),
+    new Curso("Diseño UX", "teacher.jpg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat voluptatem odit doloremque porro similique?", 5000, 3)
 ];
 
 
@@ -84,34 +85,51 @@ let agregarCards = () => {
         //Agrego los botones
         //Boton 1
         const botonCard1 = document.createElement("a");
-        botonCard1.className = "btn col-5 ";
-        botonCard1.id = "boton";
+        botonCard1.className = `btn col-5 curso${card.id}`;
+        botonCard1.id = `boton`;;
         botonCard1.textContent = "Añadir al carrito";
         botonCard1.addEventListener("click", agregarAlCarrito);
+        console.log(botonCard1);
         rowBotones.appendChild(botonCard1);
 
         //Boton 2
         const botonCard2 = document.createElement("a");
-        botonCard2.href = "#";
-        botonCard2.className = "btn col-5";
-        botonCard2.id = "boton";
+        botonCard2.className = `btn col-5 curso${card.id}`;
+        botonCard2.id = `boton`;
+        botonCard2.addEventListener("click", eliminarDelCarrito);
         botonCard2.textContent = "Eliminar del carrito";
         rowBotones.appendChild(botonCard2);
     });
 }
 
-
+//Listener para el carrito, la idea es que se muestre un modal con los cursos agregados,
+//pero todavia falta el desarrollo.
 const imgCarro = document.querySelector(".imgCarrito");
 imgCarro.addEventListener("click", function() {})
 
 
 let click = 0;
-
+//Funcion para poder agregar al carrito los diferentes cursos.
 function agregarAlCarrito() {
+    //Sumamos al icon del carrito
     const num = document.querySelector(".num");
-    console.log(click);
     click += 1;
     num.textContent = parseInt(click);
 }
+
+function eliminarDelCarrito() {
+    const num = document.querySelector(".num");
+    click -= 1;
+    if (click < 0) {
+        click = 0;
+        num.textContent = parseInt(click);
+    } else {
+        num.textContent = parseInt(click);
+    }
+
+
+}
+
+
 
 agregarCards();
